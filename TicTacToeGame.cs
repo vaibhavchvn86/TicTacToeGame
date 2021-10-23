125218d02c0a5205ab2be2bae98b556f1b8a458e
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-10-23 11:57:49 +0530

    [VAIBHAVCHAVAN]ADDED: showing for Tictactoe Game Board

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -6,7 +6,7 @@ namespace TicTacToe
 {
     public class TicTacToeGame
     {
-
+        public char[] arr;
         /// <summary>
         /// UC1 Assigns the empty space.
         /// </summary>
@@ -37,6 +37,20 @@ namespace TicTacToe
             }
             else
                 Console.WriteLine("Invalid input Choose X or O only");
+        }
+        public static void showBoard()
+        {
+            AssignEmptySpace();
+           
+            Console.WriteLine("Creating a TicTacToe Game Board");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {1}  |  {2}  |  {3}");
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {4}  |  {5}  |  {6}");
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {7}  |  {8}  |  {9}");
         }
     }
 }
