2780990a6884db2b65535f3b0bbd2eabc8f821e3
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-10-23 11:10:06 +0530

    [VAIBHAVCHAVAN]ADDED: Solution file for tictactoe game

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TicTacToe;
+
+namespace tictactoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to tictactoe");
+            TicTacToeGame.AssignEmptySpace();
+            TicTacToeGame.XorO();
+        }
+    }
+}
